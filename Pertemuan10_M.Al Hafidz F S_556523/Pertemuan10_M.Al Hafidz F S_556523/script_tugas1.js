e72f8023ccb60627f701eff877d70130bf5e2ed2
Hafidz <hafidzmpis@gmail.com>
2026-06-14 15:15:42 +0700

    Pertemuan 10

---
+++ Pertemuan10_M.Al Hafidz F S_556523/Pertemuan10_M.Al Hafidz F S_556523/script_tugas1.js
@@ -0,0 +1,7 @@
+    console.log("Halo dari JavaScript EKSTERNAL!");
+
+    // innerHTML ke elemen di HTML
+    var elemenEksternal = document.getElementById("output-eksternal");
+    elemenEksternal.innerHTML = "Sukses! Ini dari file script_tugas1.js pakai innerHTML!";
+
+    alert("Ini alert dari JavaScript EKSTERNAL!");
